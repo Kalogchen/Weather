1df9eea8f68949e652f0401e81c5d0fef4690f40
Kalog <351063439@qq.com>
2016-11-25 23:35:38 +0800

    新增数据库帮助类，以及各类对应的实体类。

---
+++ app/src/main/java/com/example/kalogchen/weather/model/County.java
@@ -0,0 +1,44 @@
+package com.example.kalogchen.weather.model;
+
+/**
+ * Created by kalogchen on 2016/11/25.
+ */
+
+public class County {
+    private int id;
+    private String countyName;
+    private String countyCode;
+    private String cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(String cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getCountyCode() {
+        return countyCode;
+    }
+
+    public void setCountyCode(String countyCode) {
+        this.countyCode = countyCode;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+}
